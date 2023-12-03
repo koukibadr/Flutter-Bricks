873b09f54de1f7a41962d9bc2362d2cbaf40a007
koukibadr <badreddinekouki1998@gmail.com>
2023-12-03 19:01:50 +0100

    create git hooks project shell scripts

--- git_hooks_setup/hooks/post_gen.dart
+++ git_hooks_setup/hooks/post_gen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add post-generation logic.
+void run(HookContext context) async {
+  await Future.wait([
+    Process.run('bash', ['-c', 'chmod ug+x scripts/*']),
+    Process.run('bash', ['-c', 'git config core.hooksPath scripts']),
+  ]);
 }
